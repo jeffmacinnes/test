a5df77f5ad28e5f992653f21b517baac60be0e0a
Jeff MacInnes <jeff.macinnes@gmail.com>
2022-02-03 00:04:40 -0800

    test root files

---
+++ google.config.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: "1Jsvdtq2u-ybpQ_Ix4thFfk6zbGkGZsR6ph3DozF4vqs",
+    filepath: "src/data/doc.json",
+  },
+  {
+    id: "1IxXSYkP61o1W6dYIDL8YKQkC43e0hgvw2mLCfQcEuDw",
+    gid: "0",
+    filepath: "src/data/banknotesData.csv",
+  },
+  {
+    id: "1IxXSYkP61o1W6dYIDL8YKQkC43e0hgvw2mLCfQcEuDw",
+    gid: "222033591",
+    filepath: "src/data/nameIDs.csv",
+  },
+];
